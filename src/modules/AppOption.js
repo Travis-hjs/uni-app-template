22049ef6d2f4a9c475a3f5c1ad37666b92dea8f3
Hjs <1141752341@qq.com>
2020-07-07 18:29:08 +0800

    注释修改

--- src/modules/AppOption.js
+++ src/modules/AppOption.js
@@ -1,17 +1,17 @@
-import StoreUserInfo from "../store/userInfo";
+import StoreUserInfo from "../store/UserInfo";
 
 export default class ModuleAppOption extends StoreUserInfo {
     /** `APP`操作信息 */
     appOption = {
-        /** `wechat`导航栏高度 */
+        /** `小程序`导航栏高度 */
         navBarHeight: 0,
-        /** `wechat`胶囊距右方间距（方保持左、右间距一致） */
+        /** `小程序`胶囊距右方间距（方保持左、右间距一致） */
         menuRight: 0, 
-        /** `wechat`胶囊距底部间距（保持底部间距一致） */
+        /** `小程序`胶囊距底部间距（保持底部间距一致） */
         menuBottom: 0, 
-        /** `wechat`胶囊高度（自定义内容可与胶囊高度保证一致） */
+        /** `小程序`胶囊高度（自定义内容可与胶囊高度保证一致） */
         menuHeight: 0,
-        /** `wechat`胶囊宽度 */
+        /** `小程序`胶囊宽度 */
         menuWidth: 0,
         /** 状态栏高度 */
         statusBarHeight: 0,
@@ -19,16 +19,23 @@ export default class ModuleAppOption extends StoreUserInfo {
         isIPhoneX: false
     }
 
-    /** 初始化`APP`操作信息 */
+    /** 
+     * 初始化`APP`操作信息 
+     * @learn 条件编译 https://uniapp.dcloud.io/platform
+    */
     initAppOption() {
         const systemInfo = uni.getSystemInfoSync();
-        // 网页中`model`只有`iPhone`字段不能这样判断
+        
+        // 网页中`model`只有`iPhone`字段，所以不能这样判断
         // this.appOption.isIPhoneX = systemInfo.model.search('iPhone X') == 0 ? true : false;
+
         const isIos = systemInfo.system.toLocaleLowerCase().includes("ios");
         const vaule = (systemInfo.screenWidth / systemInfo.screenHeight) < 0.5;
+
         this.appOption.isIPhoneX = (isIos && vaule);
         this.appOption.statusBarHeight = systemInfo.statusBarHeight;
-        // #ifdef MP-WEIXIN
+
+        // #ifdef MP
         const menuButtonInfo = uni.getMenuButtonBoundingClientRect();
         // 导航栏高度 = 状态栏到胶囊的间距（胶囊距上距离-状态栏高度） * 2 + 胶囊高度 + 状态栏高度
         this.appOption.navBarHeight = (menuButtonInfo.top - systemInfo.statusBarHeight) * 2 + menuButtonInfo.height + systemInfo.statusBarHeight;
--- src/store/userInfo.js
+++
@@ -1,9 +0,0 @@
-export default class StoreUserInfo {
-    /** 用户信息 */
-    userInfo = {
-        /** 登录凭据 */
-        token: "",
-        /** 用户手机号 */
-        phone: ""
-    }
-}
