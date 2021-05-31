ba0ac118ab96d25c24ff09a7bba60ebac8f5ad7b
黄景圣 <1141752341@qq.com>
2021-05-31 21:07:39 +0800

    demo修改

--- src/pages/taBarHome/home.vue
+++ src/pages/taBarHome/home.vue
@@ -3,15 +3,15 @@
         <button class="button_pink" @click="addCount">当前页面`count`：{{ count }}</button>
         <view>userInfo.token: {{ userInfo.token }}</view>
         <button class="button" @click="setToken">修改`userInfo.token = "123"`</button>
-        <div class="line"></div>
+        <view class="line"></view>
         <TheButton color="#07c160" @click="openForm()">打开表单验证页</TheButton>
-        <div class="line"></div>
+        <view class="line"></view>
         <image class="logo" :src="imageInfo.logo"></image>
-        <div class="line"></div>
+        <view class="line"></view>
         <TheButton :loading="loading" @click="setLoading(2)">加载 2 秒</TheButton>
-        <div class="line"></div>
+        <view class="line"></view>
         <TheButton :round="true" :height="120" color="linear-gradient(45deg, #FA8BFF 0%, #2BD2FF 52%, #2BFF88 90%)" style="width: 70%" :loading="loading" @click="setLoading(3)">圆角按钮 加载 3 秒</TheButton>
-        <div class="line"></div>
+        <view class="line"></view>
         <TheButton color="#020202" textColor="#f04e7d" :loading="loading" @click="setLoading(4)">自定义字体颜色 加载 4 秒</TheButton>
     </view>    
 </template>
