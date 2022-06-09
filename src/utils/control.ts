b37f8fc84a6d532cf2a97e637aa17f16ed335453
黄景圣 <1141752341@qq.com>
2022-06-09 15:38:18 +0800

    注释修改

--- src/utils/control.ts
+++ src/utils/control.ts
@@ -148,7 +148,7 @@ interface ScrollviewOption {
     wrapWidth: number
     /** 点击事件 */
     event: Event
-    /** 是否首次设置偏移到中心位置，设置为`true`时，只需要传入`id`即可 */
+    /** 是否主动设置偏移到中心位置，设置值时，`event`不需要传入 */
     scrollValue: number
     /** 回调 */
     callback(left: number, node: UniApp.NodeInfo): void
