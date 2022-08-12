933d12b83c7ece5b372305624270aaa78f5dda97
黄景圣 <1141752341@qq.com>
2022-08-12 14:18:29 +0800

    类型注释修改

--- src/types/index.ts
+++ src/types/index.ts
@@ -16,8 +16,8 @@ export interface TheFormRulesItem {
   type?: "number" | "array"
   /**
    * 自定义的校验规则（正则）
-   * @description 考虑到微信一些特殊的抽风机制，在微信小程序中，除 number|string|object 这几个基础类型外，其他类型是会被过滤掉，所以这里在写正则的时候，在末尾加上`.toString()`即可
-  */
+   * - 考虑到微信一些特殊的抽风机制，在微信小程序中，除`number|string|object|undefined|null`这几个基础类型外，其他类型是会被过滤掉，所以这里在写正则的时候，在末尾加上`.toString()`即可
+   */
   reg?: string // | RegExp
 }
 
